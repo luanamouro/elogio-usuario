b474fa32f530edd52bf56aece10aff121187ec87
Luana <luana.mouro@gmail.com>
2022-01-07 17:14:51 -0300

    Aula 3

---
+++ elogio usuario/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace elogio_usuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo;
+            Console.WriteLine("Digite o código do elogio:\n\n1- Elogio Profissional\n2- Elogio Físico\n3- Elogio Pessoal");
+            codigo = Convert.ToInt32(Console.ReadLine());
+
+            switch (codigo)
+            {
+                case 1:
+                    Console.WriteLine("Você escolheu ELOGIO PROFISSIONAL");
+                    break;
+                case 2:
+                    Console.WriteLine("Você escolheu ELOGIO FÍSICO");
+                    break;
+                case 3:
+                    Console.WriteLine("Você escolheu ELOGIO PESSOAL");
+                    break;
+                default:
+              
+                    Console.WriteLine("Código Inválido");
+                    break;
+
+
+
+            }
+        }
+    }
+}
